147bd4f969b48e0434632b8c117d7108fc5ab008
max <49234684+max-golubtsov@users.noreply.github.com>
2022-07-21 15:00:10 +0300

    Update result_modifier.php

--- SEO/canonical/section/result_modifier.php
+++ SEO/canonical/section/result_modifier.php
@@ -1,13 +1,13 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 /* *** Каталог *** */
 
-use \Bitrix\Main\Page\Asset;									// Класс подключения для подключения в head
+use \Bitrix\Main\Page\Asset;								// Класс для подключения в head
 
 $arSectCanonical = CIBlockSection::GetList(						// Класс для работы с разделами
 	array("SORT" => "ASC"),
 	array(
 		"IBLOCK_ID" => $arParams["IBLOCK_ID"],					// ID Инфоблока
-		"ID" => $arResult["VARIABLES"]["SECTION_ID"]),			// ID Секции
+		"ID" => $arResult["VARIABLES"]["SECTION_ID"]),				// ID Секции
 		false,
 		$arSelect = array("UF_CANONICAL_SECTION")				// Название UF свойства
 );
