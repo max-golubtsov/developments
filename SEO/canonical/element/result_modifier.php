de1cb6542096c85839118b68ab31ecabbaf69114
gremlin71 <gremlin-71@mail.ru>
2022-07-21 14:07:12 +0300

    canonical

--- SEO/canonical/element/result_modifier.php
+++ SEO/canonical/element/result_modifier.php
@@ -1,19 +1,12 @@
-<?
-/* *** Каталог *** */
-use \Bitrix\Main\Page\Asset;									// Класс подключения для подключения в head
-$arSectCanonical = CIBlockSection::GetList(						// Класс для работы с разделами
-	array("SORT" => "ASC"),
-	array(
-		"IBLOCK_ID" => $arParams["IBLOCK_ID"],					// ID Инфоблока
-		"ID" => $arResult["VARIABLES"]["SECTION_ID"]),			// ID Секции
-		false,
-		$arSelect = array("UF_CANONICAL_SECTION")				// Название UF свойства
-);
-if ($canonical = $arSectCanonical -> GetNext()):
-	// Проверка на пустоту
-	if (!empty($canonical["UF_CANONICAL_SECTION"])):
-		// Вывод
-		Asset::getInstance()->addString('<link rel="canonical" href="'.$canonical["UF_CANONICAL_SECTION"].'">');
-	endif;
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+/* *** Элемент *** */
+
+// Класс подключения для подключения в head
+use \Bitrix\Main\Page\Asset;
+
+// Проверка на пустоту
+if (!empty($arResult["PROPERTIES"]["CANONICAL_ELEMENT"]["VALUE"])):
+	// Вывод
+	Asset::getInstance()->addString('<link rel="canonical" href="'.$arResult["PROPERTIES"]["CANONICAL_ELEMENT"]["VALUE"].'">');
 endif;
 ?>
