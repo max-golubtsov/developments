b20b5427af63647dc89a2a04f47312f3ebaa55a9
gremlin71 <gremlin-71@mail.ru>
2022-07-21 13:46:18 +0300

    canonical

---
+++ SEO/canonical/result_modifier.php
@@ -0,0 +1,19 @@
+<?
+/* *** Каталог *** */
+use \Bitrix\Main\Page\Asset;									// Класс подключения для подключения в head
+$arSectCanonical = CIBlockSection::GetList(						// Класс для работы с разделами
+	array("SORT" => "ASC"),
+	array(
+		"IBLOCK_ID" => $arParams["IBLOCK_ID"],					// ID Инфоблока
+		"ID" => $arResult["VARIABLES"]["SECTION_ID"]),			// ID Секции
+		false,
+		$arSelect = array("UF_CANONICAL_SECTION")				// Название UF свойства
+);
+if ($canonical = $arSectCanonical -> GetNext()):
+	// Проверка на пустоту
+	if (!empty($canonical["UF_CANONICAL_SECTION"])):
+		// Вывод
+		Asset::getInstance()->addString('<link rel="canonical" href="'.$canonical["UF_CANONICAL_SECTION"].'">');
+	endif;
+endif;
+?>
