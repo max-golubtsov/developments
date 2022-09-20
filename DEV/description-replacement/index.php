17a45cf8638b7341a0b5ad2eccf540f33182a588
gremlin71 <gremlin-71@mail.ru>
2022-09-20 15:36:35 +0300

    load

--- DEV/description-replacement/index.php
+++ DEV/description-replacement/index.php
@@ -1,16 +1,21 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+// require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 
-CModule::IncludeModule("iblock");
+// Для выгрузки в init.php
+// AddEventHandler("catalog", "OnSuccessCatalogImport1C","detailTextUpdate");
+// function detailTextUpdate() {
 
-$rsEl = CIBlockElement::GetList(Array(), Array("IBLOCK_ID"=>1));
+    CModule::IncludeModule("iblock");
 
-while($arEl = $rsEl->GetNext()){ 
-	$el = new CIBlockElement;
-	$arFields = Array();
-	if($arEl["PREVIEW_TEXT"]){
-		$arFields['DETAIL_TEXT'] = $arEl["PREVIEW_TEXT"];
-		$el->Update($arEl["ID"], $arFields);
-	}
-}
+    $rsEl = CIBlockElement::GetList(Array(), Array("IBLOCK_ID"=>1));
+    
+    while($arEl = $rsEl->GetNext()){ 
+        $el = new CIBlockElement;
+        $arFields = Array();
+        if($arEl["PREVIEW_TEXT"]){
+            $arFields['DETAIL_TEXT'] = $arEl["PREVIEW_TEXT"];
+            $el->Update($arEl["ID"], $arFields);
+        }
+    }
+// }
 ?>
